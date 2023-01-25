de60d4d7ac2753e2f1235c0b4b72c317e9f74538
Papuna Tsereteli <117572811+skameri@users.noreply.github.com>
2023-01-25 17:51:51 +0400

    Update App.vue

--- haha/src/App.vue
+++ haha/src/App.vue
@@ -3,10 +3,9 @@
   <button @click = "showHide" >
     <span v-if = "isHidden">Hide</span>
     <span v-if = "!isHidden">Show</span>
-  
   </button>
   <ul v-if = this.isHidden>
-    <li v-for = "a in list">{{a}}</li>
+    <li v-for = "a in list">><a :href="a.link">{{ a.name }}</a> </li>
   </ul>
 </template>
 
@@ -15,14 +14,16 @@
 
 
 
-
-
 export default {
 
   data(){
         return{
             list: [
-              "Home","Game","Download","About us"
+              {id: 1 , name: "Home", link: "https://stackoverflow.com/questions/40899532/how-to-pass-a-value-from-vue-data-to-href"},
+              {id: 2 , name: "Game",link: "https://github.com/skameri/gtxovimushave"},
+              {id: 3 , name: "Download",link: "https://us06web.zoom.us/postattendee?mn=2kFMiXB3K5_1iia5Mq_eziO8Fks6e5f0m-tg.z8SxurzTCDMr21fh"},
+              {id: 4 , name: "About us",link: "https://vuejs.org/guide/quick-start.html#creating-a-vue-application"},
+              
             ],
             isHidden : true,
             
@@ -49,6 +50,12 @@ li{
   display: inline-block;
   list-style: none;
   color: blue;
+}
+
+button{
+  margin-top: 40px;
+  width: 60px;
+  height: 21px;
 }
 
 </style>
