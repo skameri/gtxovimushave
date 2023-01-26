9a5420445315eb473503fd7c46af9a50dd932ee5
Papuna Tsereteli <117572811+skameri@users.noreply.github.com>
2023-01-26 15:57:39 +0400

    Update App.vue

--- haha/src/App.vue
+++ haha/src/App.vue
@@ -1,12 +1,15 @@
 
 <template>
+
   <button @click = "showHide" >
     <span v-if = "isHidden">Hide</span>
     <span v-if = "!isHidden">Show</span>
   </button>
   <ul v-if = this.isHidden>
-    <li  v-for = "  a in list" :key = "a.id" @click.prevent = "guka"><a :href="a.link">{{ a.name }}</a> </li>
+    <li  v-for = "  a in list" key = "a.id" @click.prevent = "guka" ><a :href="a.link"  :id=a.info>{{ a.name }} </a> </li>
+    <div >{{ info }}</div>
   </ul>
+  
 </template>
 
 
@@ -19,13 +22,14 @@ export default {
   data(){
         return{
             list: [
-              {id: 1 , name: "Home", link: "https://stackoverflow.com/questions/40899532/how-to-pass-a-value-from-vue-data-to-href"},
-              {id: 2 , name: "Game",link: "https://github.com/skameri/gtxovimushave"},
-              {id: 3 , name: "Download",link: "https://us06web.zoom.us/postattendee?mn=2kFMiXB3K5_1iia5Mq_eziO8Fks6e5f0m-tg.z8SxurzTCDMr21fh"},
-              {id: 4 , name: "About us",link: "https://vuejs.org/guide/quick-start.html#creating-a-vue-application"},
+              {id: 1 , name: "Home", link: "https://stackoverflow.com/questions/40899532/how-to-pass-a-value-from-vue-data-to-href", info:"random information home"},
+              {id: 2 , name: "Game",link: "https://github.com/skameri/gtxovimushave",info:"random information game"},
+              {id: 3 , name: "Download",link: "https://us06web.zoom.us/postattendee?mn=2kFMiXB3K5_1iia5Mq_eziO8Fks6e5f0m-tg.z8SxurzTCDMr21fh",info:"random information download"},
+              {id: 4 , name: "About us",link: "https://vuejs.org/guide/quick-start.html#creating-a-vue-application",info:"random information about us"},
               
             ],
             isHidden : true,
+            info:"",
             
         }
     },
@@ -35,7 +39,9 @@ export default {
         this.isHidden = !this.isHidden
       },
       guka(e){
-        console.log(e.target.innerHTML+  " Link: "+e.target.href )
+        this.info = e.target.getAttribute("id")
+     
+        console.log(e.target.getAttribute("id"))
       }
     }
  
