e41f51ea01490b8a9b20c04c3d59797a6f2b5aed
Papuna Tsereteli <117572811+skameri@users.noreply.github.com>
2023-01-27 17:46:27 +0400

    Update App.vue

--- haha/src/App.vue
+++ haha/src/App.vue
@@ -1,129 +1,53 @@
 <template>
-  <div v-if = "visible" >
-    <form v-bind:style="{ display: none }" >
-      <label>
-        <input placeholder="Your school e-mail"  type="email" v-model="email" class="form-input" />
-      </label>
-      <br />
-      <label>
-        <input placeholder="Group number"  v-model="group" class="form-input" />
-      </label>
-      <br />
-      <button @click="submitForm" class="form-button">Start the test</button>
+  <div>
+    <form @submit.prevent="addItem">
+      <input v-model="newItem" placeholder="Add item to list">
+      <button type="submit">Add</button>
     </form>
+    <ul>
+      <li v-for="item in items" :key="item">
+        <input type="checkbox" v-model="item.checked" @click="checkItem(item)"/>
+        {{ item.text }}
+      </li>
+    </ul>
+    <button @click="deleteCheckedItems">Delete Checked Items</button>
+    <h2>Checked Items</h2>
+    <ul>
+      <li v-for="item in checkedItems" :key="item.text">{{ item.text }}</li>
+    </ul>
+    <h2>Deleted Items</h2>
+    <ul>
+      <li v-for="item in deletedItems" :key="item.text">{{ item.text }}</li>
+    </ul>
   </div>
-
-  <div v-if = !visible class="test-page">
-    <div class="gurami">
-      {{ ragaca }}
-
-    </div>
-    <input v-bind:style="{backgroundColor: color }"  v-for = "a in questions" type="text"  :id=a.question placeholder="Answer here" class="answer-button" :v-model = answer1  @click = showQuestion >
-    <p>Exam for group N:{{ group }}</p>
-    <p>press the input to check the question</p>
-    <button   class="form-button">Submit</button>
-
-
-  </div>
-
- 
-  
-
-
 </template>
-
 <script>
 export default {
   data() {
     return {
-      email: null,
-      group: null,
-      visible: true,
-      answer1: null,
-      answer2: null,
-      answer3: null,
-      ragaca: null,
-      color: null,
-      questions: [
-        {id: 1,question: "The side of a square is 5 meters,find area", answer: "25"},
-        {id: 2,question: "Which number is larger, -5 or -3", answer: "-3"},
-        {id: 3,question: "What is 85% of 21", answer: "17.85"}
-      ]
+      newItem: '',
+      items: [],
+      checkedItems: [],
+      deletedItems: []
     }
   },
   methods: {
-    submit(){
-      
-
+    addItem() {
+      this.items.push({ text: this.newItem, checked: false });
+      this.newItem = '';
     },
-    showQuestion(e){
-      this.ragaca = e.target.id
-
+    checkItem(item) {
+      if (item.checked) {
+        this.checkedItems.push(item);
+      } else {
+        this.checkedItems = this.checkedItems.filter(i => i !== item);
+      }
     },
-    submitForm() {
-      if(this.group!=null && this.email!=null){
-        this.visible = false
-        console.log(this.email)
-
-      }
-      else{
-        alert("Enter your e-mail and group number")
-      }
-     
-   
+    deleteCheckedItems() {
+      this.deletedItems = [...this.deletedItems, ...this.checkedItems];
+      this.items = this.items.filter(item => !item.checked);
+      this.checkedItems = []
     }
   }
 }
 </script>
-
-<style>
-.form-input {
-  width: 300px;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 10px;
-}
-.answer-button{
-  width: 300px;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 10px;
-  display:block ;
-
-}
-.gurami{
-  font-size: 14px;
-  font-weight: bold;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
-.password{
-  display: inline;
-  margin: -10px;
-}
-
-.form-button {
-  width: 200px;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.form-button:hover {
-  background-color: #45a049;
-}
-
-body{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #84D2C5;
-}
-</style>
